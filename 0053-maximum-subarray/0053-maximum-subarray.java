d54d041fa6a3f7f43fe8456e2724f174862b06b9
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-01-26 20:59:04 +0530

    Time: 1 ms (99.6%), Space: 77.1 MB (75.97%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(cs < 0)
+                cs = 0;
+            cs += nums[i];
+            if(cs > ms)
+                ms = cs;
+            
+        }
+        return ms;
+    }
+}
