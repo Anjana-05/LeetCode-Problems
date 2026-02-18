4995658afd6a5d65266f829dc62260bcebe55365
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-02-18 21:35:49 +0530

    Time: 0 ms (100%), Space: 44.5 MB (16.45%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -11,7 +11,7 @@ class Solution {
                 return mid;
             else if(nums[mid -1] > nums[mid])
                 high = mid - 1;
-            else if(nums[mid + 1] > nums[mid])
+            else
                 low = mid + 1;
         }
         return 0;
