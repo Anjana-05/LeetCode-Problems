834d97ff4e65fdfee0848d1abfd0efd3528a2916
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-02-19 21:13:08 +0530

    Time: 1 ms (99.6%), Space: 48.5 MB (22.34%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] rowAndMaximumOnes(int[][] mat) {
         int[] rowsum = new int[mat.length];
         int maxiOnes[] = new int[2];
-        int maxsum=0;
         for(int i = 0 ; i < mat.length ; i++){
             int sum =0;
             for(int j = 0 ; j < mat[0].length; j++){
