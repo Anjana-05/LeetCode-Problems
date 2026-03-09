772c8465765973089ab22c90090df7ecc87e5df3
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-03-09 20:53:54 +0530

    Time: 5 ms (35.69%), Space: 47.1 MB (14.61%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,75 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
-        int res = 0;
-        int len = s.length();
-        for(int i = 0 ; i < len ; i++ ){
-            char c = s.charAt(i);
-            switch(c){
-                case 'I':
-                    if(i < len - 1){
-                        char next = s.charAt(i+1);
-                        if(next == 'V'){
-                            res += 3;
-                            i++;
-                        }
-                        else if(next == 'X'){
-                            res += 8;
-                            i++;
-                        }
-                    }
-                    res += 1;
-                    break;
+        int res =0;
+        HashMap<Character, Integer> roman = new HashMap<>();
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
 
-                case 'V':
-                    res += 5;
-                    break;
-                
-                case 'X':
-                    if(i < len-1){
-                        char next = s.charAt(i+1);
-                        if(next == 'L'){
-                            res += 30;
-                            i++;
-                        }
-                        else if(next == 'C'){
-                            res += 80;
-                            i++;
-                        }
-                    }
-                    res += 10;
-                    break;
-
-                case  'L':
-                    res += 50;
-                    break;
-
-                case 'C':
-                    if(i < len-1){
-                        char next = s.charAt(i+1);
-                        if(next == 'D'){
-                            res += 300;
-                            i++;
-                        }
-                        else if(next == 'M'){
-                            res += 800;
-                            i++;
-                        }
-                    }
-                    res += 100;
-                    break;
-
-                case 'D':
-                    res += 500;
-                    break;
-
-                case 'M':
-                    res += 1000;
-                    break;
-                
-                default:
-                    res += 0;
-            }
+        for(int i = 0 ; i < s.length() -1 ; i++){
+            if(roman.get(s.charAt(i)) < roman.get(s.charAt(i+1)))
+                res -= roman.get(s.charAt(i));
+            else
+                res += roman.get(s.charAt(i));
         }
+        res += roman.get(s.charAt(s.length()-1));
         return res;
     }
 }
