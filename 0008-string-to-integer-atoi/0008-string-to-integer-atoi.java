82287e47672fd2c4f80cfeb00ac51ea299dcbbf6
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-05-19 21:23:16 +0530

    Time: 1 ms (100%), Space: 43.7 MB (74.64%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,28 +1,24 @@
 class Solution {
     public int myAtoi(String s) {
-        int res = 0, sign = 1, index = 0;
         s = s.trim();
-        if(s.length() == 0)
-            return 0;
-        if(s.charAt(0) == '-'){
-            sign = -1;
-            index++;
-        }else if(s.charAt(0) == '+'){
-            sign = 1;
+        if(s.length() == 0) return 0;
+        int index = (s.charAt(0) == '-' || s.charAt(0) == '+') ? 1 : 0;
+        int sign = (s.charAt(0) == '-') ? -1 : 1;
+        long res = 0;
+        while(index < s.length() && s.charAt(index) == '0'){
             index++;
         }
-        while(index < s.length() && s.charAt(index) == '0')
-            index++;
         while(index < s.length()){
-            if(s.charAt(index) < '0' ||  s.charAt(index) > '9')
-                return sign*res;
-            int digit = s.charAt(index) - '0';
-            if(res > Integer.MAX_VALUE / 10|| ( res == Integer.MAX_VALUE / 10 && digit > 7))
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            res = res * 10 + digit;
+            char c = s.charAt(index);
+            if(Character.isDigit(c))
+                res  = res * 10 + (c - '0');
+            else
+                break;
             index++;
+            if(res * sign >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            else if(res * sign <= Integer.MIN_VALUE) return Integer.MIN_VALUE; 
         }
-        
-        return sign * res;
+
+        return (int)res * sign;
     }
 }
