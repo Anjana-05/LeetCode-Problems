847035687db40be13422b4be07b78d4064095778
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-06-16 22:47:45 +0530

    Time: 22 ms (20.64%), Space: 45.9 MB (46%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i = 0 ; i < nums.length; i++){
 
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int i = 0 ; i < nums.length-3; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
-    
-            for(int j = i+1 ; j < nums.length; j++){
+            for(int j = i+1; j < nums.length-2; j++){
                 if(j > i+1 && nums[j] == nums[j-1]) continue;
 
-                int k = j + 1;
-                int l = nums.length-1;
-
-                while(k < l){
-                    long  sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
-
+                int l = j+1;
+                int r = nums.length-1;
+                while(l < r){
+                    long sum = (long)nums[i] + nums[j] + nums[l] + nums[r];
                     if(sum == target){
-                        res.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
-
-                    while(k < l && nums[k] == nums[k+1]) k++;
-                    while(k < l && nums[l] == nums[l-1]) l--;
-
-                    k++; l--;
+                        res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+                        while(l < r && nums[l] == nums[l+1]) l++;
+                        while(l < r && nums[r] == nums[r-1]) r--;
+                        l++;
+                        r--;
                     }
-
-                    else if (sum < target) k++;
-
-                    else l--;
+                    else if(sum > target)
+                        r--;
+                    else
+                        l++;
                 }
-            } 
+            }
         }
         return res;
     }
