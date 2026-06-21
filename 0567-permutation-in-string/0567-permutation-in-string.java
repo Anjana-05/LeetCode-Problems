fa24ffc5f28216f8153537325c00dd56528af5fe
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-06-21 23:47:54 +0530

    Time: 24 ms (30.18%), Space: 44.9 MB (30.58%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,37 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        int l =0 , r = 0;
 
-        int l =0, r = 0;
-
-
-        if(s1.length() > s2.length())
-            return false;
+        HashMap<Character, Integer> hm = new HashMap<>();
 
         for(int i = 0 ; i < s1.length() ; i++){
-            freq1[s1.charAt(i)-'a']++;
+            hm.put(s1.charAt(i) , hm.getOrDefault(s1.charAt(i),0)+1);
         }
 
-        for(r = 0 ; r < s2.length() ; r++){
-            freq2[s2.charAt(r)-'a']++;
+        int count = s1.length();
 
-            if((r-l+1) > s1.length()){
-                freq2[s2.charAt(l)-'a']--;
+        while(r < s2.length()){
+            
+            int val = hm.getOrDefault(s2.charAt(r),0);
+            if(val > 0) count--;
+            hm.put(s2.charAt(r), val-1);
+
+            if(count == 0) return true;
+
+            if((r-l+1) == s1.length()){
+                if(hm.get(s2.charAt(l)) >= 0) count++;
+                hm.put(s2.charAt(l), hm.get(s2.charAt(l))+1);
                 l++;
             }
 
-            if((r-l+1) == s1.length()){
-                boolean flag = true;
-                for(int i = 0; i < 26 ; i++){
-                    if(freq1[i] != freq2[i]){
-                        flag = false;
-                        break;
-                    }
-                }
-
-                if(flag) return true;
-            }
+            r++;
         }
         return false;
     }
