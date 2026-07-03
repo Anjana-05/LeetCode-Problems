18da731bc1f894126a690e0c5e3de7d237c50364
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-07-03 22:43:04 +0530

    Time: 0 ms (100%), Space: 48.2 MB (30.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        int n = nums.length-1;
-        res[0] = firstFind(nums,n, target);
-        res[1] = lastFind(nums,n,target);
-        return res;
-    }
-
-    public int firstFind(int[] nums, int n, int target){
-        int low = 0, high = n;
-        int pos = -1;
-        while(low <= high){
-            int mid = (low+high) /2;
+        int left = 0, right = nums.length - 1;
+        int firstOccurence = -1;
+        int lastOccurence = -1;
+        while(left <= right){
+            int mid = left + (right- left)/2;
             if(nums[mid] == target){
-                pos = mid;
-                high = mid-1; 
+                firstOccurence = mid;
+                right = mid-1;
             }
-            else if(nums[mid] > target)
-                high = mid-1;
-            else
-                low = mid+1;
-        }
-        return pos;
-    }
 
-    public int lastFind(int[] nums, int n, int target){
-        int low = 0, high = n;
-        int pos = -1;
-        while(low <= high){
-            int mid = (low+high) /2;
-            if(nums[mid] == target){
-                pos = mid;
-                low = mid+1; 
-            }
             else if(nums[mid] > target)
-                high = mid-1;
+                right = mid-1;
             else
-                low = mid+1;
+                left = mid + 1;
         }
-        return pos;
+
+
+        if(nums.length == 0 || firstOccurence == -1) return new int[]{firstOccurence, lastOccurence};
+        left = firstOccurence;
+        right = nums.length-1;
+        while(left <= right){
+            int mid = left + (right- left)/2;
+            if(nums[mid] == target){
+                lastOccurence = mid;
+                left = mid + 1;
+            }
+
+            else if(nums[mid] > target)
+                right = mid-1;
+            else
+                left = mid + 1;
+        }
+        return new int[]{firstOccurence, lastOccurence};
     }
 }
