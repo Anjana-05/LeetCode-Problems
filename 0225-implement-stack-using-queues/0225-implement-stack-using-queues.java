a4f34d6d4055fc063daa89548feb9bf2fd1052e8
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-07-31 21:27:56 +0530

    Time: 0 ms (100%), Space: 42.6 MB (82.6%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -7,20 +7,26 @@ class MyStack {
     }
     
     public void push(int x) {
-        q2.offer(x);
-        while(!q1.isEmpty())
-            q2.offer(q1.poll());
-
-        while(!q2.isEmpty())
-            q1.offer(q2.poll());
+        q1.offer(x);
     }
     
     public int pop() {
-        return q1.poll();
+        while(q1.size() > 1)
+            q2.offer(q1.poll());
+        int val = q1.poll();
+        q1 = q2;
+        q2 = new LinkedList<>();
+        return val;
     }
     
     public int top() {
-        return q1.peek();
+        while(q1.size() > 1)
+            q2.offer(q1.poll());
+        int val = q1.peek();
+        q2.offer(val);
+        q1 = q2;
+        q2 = new LinkedList<>();
+        return val;
     }
     
     public boolean empty() {
